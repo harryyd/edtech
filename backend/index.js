f4881da1f7d8ece5e53ea46f128a234735f7b686
harshit yadav <harshityd2912002@gmail.com>
2025-12-16 17:39:47 +0530

    index file added

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+const PORT = 8000;  
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running on port ${PORT}`) ;
+}) ; 
